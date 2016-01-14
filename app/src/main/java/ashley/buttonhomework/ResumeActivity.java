b19b14271f156beb90c2e758985e16d6521f31d8
Ashley Lau <ablau@stanford.edu>
2016-01-14 13:51:14 -0800

    Committing

--- app/src/main/java/ashley/buttonhomework/ResumeActivity.java
+++ app/src/main/java/ashley/buttonhomework/ResumeActivity.java
@@ -11,18 +11,22 @@ public class ResumeActivity extends AppCompatActivity {
     private TextView niceInfo;
 
 
-
+    /** When this activity is created, it takes the intent from the MainActivity class,
+     * and puts that information in the widgets on the new activity screen
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
 
-
+        //Initializes the textviews.
         name = (TextView) findViewById(R.id.nameText);
         graduYear = (TextView) findViewById(R.id.graduationText);
         personsMajor = (TextView) findViewById(R.id.majorText);
         niceInfo = (TextView) findViewById(R.id.miscText);
 
+        //Intent from MainActivity class is received:
         name.setText(getIntent().getStringExtra("User Name"));
         String graduationMsg = "Class of " + getIntent().getStringExtra("Graduation Year");
         graduYear.setText(graduationMsg);
